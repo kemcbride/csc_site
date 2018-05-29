e3acd4ad8c4fdd62410f388f77f2b477f8915b3c
kemcbride <kemcbride28@gmail.com>
2018-05-29 00:37:25 -0400

    this is pretty much broken but i am not sure how to fix

--- create_tables.py
+++ create_tables.py
@@ -1,5 +1,5 @@
 from db.models import Song, Chart, Score, MYSQL_DB
 
 if __name__ == '__main__':
-    with MYSQL_DB:
+    with MYSQL_DB.atomic():
         MYSQL_DB.create_tables([Song, Chart, Score])
--- load_stats_into_tables.py
+++ load_stats_into_tables.py
@@ -16,51 +16,59 @@ if __name__ == '__main__':
 
     # -1 for TopScores
     # -3 for RecentScores ( ??? ) (maybe? i don't remember???)
+    records = []
     for song in root[-1].getchildren():
         try:
             hs = HighScore(song)
+            records.append(hs)
         except KeyError:
             continue
 
-        with MYSQL_DB:
-            this_song = Song.create(
-                    title=hs.song_title,
-                    length=hs.song_length
-                    )
+    with MYSQL_DB.atomic():
+        Song.replace_many([
+            {
+                'title': hs.song_title,
+                'length': hs.song_length
+                }
+            for hs in records]).execute()
 
-        with MYSQL_DB:
-            this_chart = Chart.create(
-                    song_id=this_song,
-                    title=hs.difficulty,
-                    num_taps=hs.radar['Taps'],
-                    num_jumps=hs.radar['Jumps'],
-                    num_holds=hs.radar['Holds'],
-                    num_mines=hs.radar['Mines'],
-                    num_rolls=hs.radar['Rolls'],
-                    num_hands=hs.radar['Hands'],
-                    )
+    with MYSQL_DB.atomic():
+        Chart.replace_many([
+            {
+                'song_id': hs.song_title,
+                'title': hs.difficulty,
+                'num_taps': hs.radar['Taps'],
+                'num_jumps': hs.radar['Jumps'],
+                'num_holds': hs.radar['Holds'],
+                'num_mines': hs.radar['Mines'],
+                'num_rolls': hs.radar['Rolls'],
+                'num_hands': hs.radar['Hands'],
+            } for hs in records
+            ]).execute()
 
-        with MYSQL_DB:
-            this_score = Score.create(
-                    grade=hs.grade,
-                    percent=hs.pct_score,
-                    modifiers=hs.modifiers,
+    with MYSQL_DB.atomic():
+        Score.replace_many([
+            {
+                'grade': hs.grade,
+                'percent': hs.pct_score,
+                'modifiers': hs.modifiers,
+                'num_fantastic': hs.tapnotes['Marvelous'],
+                'num_excellent': hs.tapnotes['Perfect'],
+                'num_great': hs.tapnotes['Great'],
+                'num_decent': hs.tapnotes['Good'],
+                'num_wayoff': hs.tapnotes['Boo'],
+                'num_miss': hs.tapnotes['Miss'],
 
-                    num_fantastic=hs.tapnotes['Marvelous'],
-                    num_excellent=hs.tapnotes['Perfect'],
-                    num_great=hs.tapnotes['Great'],
-                    num_decent=hs.tapnotes['Good'],
-                    num_wayoff=hs.tapnotes['Boo'],
-                    num_miss=hs.tapnotes['Miss'],
-
-                    # foreign key things...
-                    # since composite, peewee won't do it for me.
-                    song_id=this_song,
-                    title=hs.difficulty,
-                    num_taps=hs.radar['Taps'],
-                    num_jumps=hs.radar['Jumps'],
-                    num_holds=hs.radar['Holds'],
-                    num_mines=hs.radar['Mines'],
-                    num_rolls=hs.radar['Rolls'],
-                    num_hands=hs.radar['Hands'],
-                    )
+                # foreign key things...
+                # since composite, peewee won't do it for me.
+                'song_id': hs.song_title,
+                'title': hs.difficulty,
+                'num_taps': hs.radar['Taps'],
+                'num_jumps': hs.radar['Jumps'],
+                'num_holds': hs.radar['Holds'],
+                'num_mines': hs.radar['Mines'],
+                'num_rolls': hs.radar['Rolls'],
+                'num_hands': hs.radar['Hands'],
+                }
+            for hs in records
+            ]).execute()
